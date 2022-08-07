2cf44a7fbb226367f0e380712a5fafe8e32c7317
Conrad <jugj@users.noreply.github.com>
2022-08-07 10:07:41 +0200

    Änderungen von Conrad um 10:07:41

--- Assets/weiter.cs
+++ Assets/weiter.cs
@@ -2,23 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pause : MonoBehaviour
+public class weiter : MonoBehaviour
 {
     public GameObject weiterknopf;
     // Start is called before the first frame update
     void Start()
     {
-        
+        weiterknopf.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    
     }
-    public void pauseSpiel()
+    public void wieterSpiel()
     {
-        Time.timeScale = 0;
-        weiterknopf.SetActive(true);
+        Time.timeScale = 1;
+        weiterknopf.SetActive(false);
     }
 }
